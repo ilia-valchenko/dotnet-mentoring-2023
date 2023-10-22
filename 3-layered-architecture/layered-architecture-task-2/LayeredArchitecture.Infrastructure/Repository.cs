0c72662955f6c3fc891bbbf654af76e1a3d6800e
ivalchanka <i.valchenk@hotmail.com>
2023-10-22 21:09:10 +0400

    Added Infrastructure layer.

---
+++ 3-layered-architecture/layered-architecture-task-2/LayeredArchitecture.Infrastructure/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LayeredArchitecture.Domain.Entities;
+using LayeredArchitecture.UseCase;
+
+namespace LayeredArchitecture.Infrastructure
+{
+    public class Repository : IRepository
+    {
+        public Task CreateAsync(Category category, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Category>> GetAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Category category, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
