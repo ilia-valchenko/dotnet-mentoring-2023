ffda928fab6c3e8022c91cf52682438acdcd5d47
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-10-23 22:27:33 +0400

    Implemented method for creating a new category.

--- 3-layered-architecture/layered-architecture-task-2/LayeredArchitecture.Infrastructure/Repository.cs
+++ 3-layered-architecture/layered-architecture-task-2/LayeredArchitecture.Infrastructure/Repository.cs
@@ -20,15 +20,67 @@ namespace LayeredArchitecture.Infrastructure
             _connectionString = connectionString;
         }
 
-        public Task CreateAsync(Category category, CancellationToken cancellationToken)
+        public async Task CreateAsync(Category category, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using var connection = new SqliteConnection(_connectionString);
+            connection.Open();
+
+            using var transaction = connection.BeginTransaction();
+
+            try
+            {
+                int numberOfAffectedRows = await connection.ExecuteAsync(
+                    "INSERT INTO Category (Id, Name, ImageUrl, ParentCategoryId) VALUES (@Id, @Name, @ImageUrl, @ParentCategoryId);",
+                    new
+                    {
+                        Id = category.Id.ToString(),
+                        Name = category.Name,
+                        ImageUrl = category.ImageUrl?.UrlText,
+                        ParentCategoryId = category.ParentCategoryId?.ToString()
+                    },
+                    transaction);
+
+                // Insert related objects.
+                foreach (var product in category.Products)
+                {
+                    numberOfAffectedRows += await connection.ExecuteAsync(
+                    "INSERT INTO Product (Id, CategoryId, Name, Description, ImageUrl, Price, Amount) " +
+                    "VALUES (@Id, @CategoryId, @Name, @Description, @ImageUrl, @Price, @Amount);",
+                    new
+                    {
+                        Id = product.Id.ToString(),
+                        CategoryId = product.CategoryId.ToString(),
+                        Name = product.Name,
+                        Description = product.Description,
+                        ImageUrl = product.ImageUrl?.UrlText,
+                        Price = product.Price,
+                        Amount = product.Amount
+                    },
+                    transaction);
+                }
+
+                if (numberOfAffectedRows < category.Products.Count + 1)
+                {
+                    throw new Exception("Failed to insert the new category.");
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+
+                throw new Exception(
+                    "Failed to insert the new category. " +
+                    $"Serialized category: {JsonConvert.SerializeObject(category)}.",
+                    ex);
+            }
         }
 
         public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
             using var connection = new SqliteConnection(_connectionString);
-            var numberOfAffectedRows = await connection.ExecuteAsync("DELETE FROM Category WHERE Id = @Id", new { Id = id.ToString() });
+            var numberOfAffectedRows = await connection.ExecuteAsync("DELETE FROM Category WHERE Id = @Id;", new { Id = id.ToString() });
 
             if (numberOfAffectedRows < 1)
             {
@@ -39,14 +91,14 @@ namespace LayeredArchitecture.Infrastructure
         public async Task<Category> GetAsync(Guid id, CancellationToken cancellationToken)
         {
             using var connection = new SqliteConnection(_connectionString);
-            var tours = await connection.QueryAsync<Category>("SELECT * FROM Category WHERE Id = @Id", new { Id = id.ToString() });
+            var tours = await connection.QueryAsync<Category>("SELECT * FROM Category WHERE Id = @Id;", new { Id = id.ToString() });
             return tours.FirstOrDefault();
         }
 
         public async Task<IList<Category>> GetAsync(CancellationToken cancellationToken)
         {
             using var connection = new SqliteConnection(_connectionString);
-            var tours = await connection.QueryAsync<Category>("SELECT * FROM Category");
+            var tours = await connection.QueryAsync<Category>("SELECT * FROM Category;");
             return tours.ToList();
         }
 
@@ -55,8 +107,9 @@ namespace LayeredArchitecture.Infrastructure
             using var connection = new SqliteConnection(_connectionString);
 
             var numberOfAffectedRows = await connection.ExecuteAsync(
-                "UPDATE Category SET Name = @Name, ImageUrl = @ImageUrl, ParentCategoryId = @ParentCategoryId WHERE Id = @Id",
-                new {
+                "UPDATE Category SET Name = @Name, ImageUrl = @ImageUrl, ParentCategoryId = @ParentCategoryId WHERE Id = @Id;",
+                new
+                {
                     Id = category.Id.ToString(),
                     Name = category.Name,
                     ImageUrl = category.ImageUrl?.UrlText,
