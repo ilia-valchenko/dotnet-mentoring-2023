3c2f39180011f373e2899658af1f28322980a505
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-11-11 23:49:07 +0400

    Created database.

--- 3-layered-architecture/layered-architecture-task-1/recreate-database.sql
+++ 3-layered-architecture/layered-architecture-task-1/recreate-database.sql
@@ -1,19 +1,14 @@
-CREATE TABLE Category
+CREATE TABLE Cart
 (
-    Id NVARCHAR(36) PRIMARY KEY,
-    Name NVARCHAR(50) NOT NULL,
-    ImageUrl TEXT NULL,
-    ParentCategoryId NVARCHAR(36) NULL
+    Id NVARCHAR(250) PRIMARY KEY,
+    Quantity INT DEFAULT 0
 );
 
-CREATE TABLE Product
+CREATE TABLE CartItem
 (
-    Id NVARCHAR(36) PRIMARY KEY,
-    CategoryId NVARCHAR(36) NOT NULL,
-    Name NVARCHAR(50) NOT NULL,
-	ImageUrl TEXT NULL,
-    Description TEXT NULL,
-    Price NUMERIC NOT NULL,
-    Amount INT NOT NULL,
-    FOREIGN KEY(CategoryId) REFERENCES Category(Id)
+    Id NVARCHAR(250) PRIMARY KEY,
+    Name NVARCHAR(250),
+    Description NVARCHAR(250),
+    CartId NVARCHAR(36) NOT NULL,
+    FOREIGN KEY(CartId) REFERENCES Cart(Id)
 );
