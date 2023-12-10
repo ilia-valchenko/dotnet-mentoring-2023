13a2244e9ad79159d972a053eb7ad38a012b3aa5
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-12-10 17:41:35 +0400

    Updated the issuer.

--- 6-authentication-and-authorization/catalog-rest-api/RestfulWebApi.Api/Constants/Constants.cs
+++ 6-authentication-and-authorization/catalog-rest-api/RestfulWebApi.Api/Constants/Constants.cs
@@ -3,6 +3,6 @@
 public static class Constants
 {
     public const string SecretKey = "my-custom-secret-key-N5jqsihSbLqqmNqBWJX5wxvF9ZnyBylg";
-    public const string Issuer = "http://localhost:5244/";
+    public const string Issuer = "https://localhost:44367/";
     public const string Audience = "http://localhost:5000/";
 }
