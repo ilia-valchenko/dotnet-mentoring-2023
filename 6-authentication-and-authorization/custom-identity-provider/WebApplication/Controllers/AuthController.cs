a10f2ceee15996526714e85025f5d6bc00cef11c
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-12-12 00:28:52 +0400

    Pushed some changes.

--- 6-authentication-and-authorization/custom-identity-provider/WebApplication/Controllers/AuthController.cs
+++ 6-authentication-and-authorization/custom-identity-provider/WebApplication/Controllers/AuthController.cs
@@ -1,15 +1,36 @@
-﻿using IdentityServer.ViewModels;
+﻿using System.Security.Cryptography;
+using IdentityServer.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using static IdentityModel.OidcConstants;
+using static IdentityServer4.Models.IdentityResources;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.WebRequestMethods;
 
 namespace IdentityServer.Controllers;
 
 public class AuthController : Controller
 {
-    public IActionResult Login()
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
     {
-        return View();
+        // *** Example of the Return URL ***
+        // /connect/authorize/callback?client_id=my_client_id_mvc
+        //& redirect_uri=https%3A%2F%2Flocalhost%3A7240%2Fsignin-oidc
+        //& response_type=code
+        //& scope=openid%20profile
+        //& code_challenge=74ut48tsVsebW9k9UAiR8YJK0NitTONmlEFyqScCAAw
+        //& code_challenge_method=S256
+        //& response_mode=form_post
+        //& nonce=638379231249474333.NDk5YjM3MGEtZTgwMy00Y2JjLWI0NDYtOGYzZGY4ZDZjZjRkOGU1
+        //& state=CfDJ8Gbrtnk6qZFAsrdxVOBx3dVok4BKle1zRKgTVU9LBpU8dWEDgdRVjkRRSTPotbmM0ve
+
+        return View(new LoginViewModel
+        {
+            ReturnUrl = returnUrl
+        });
     }
 
+    [HttpPost]
     public IActionResult Login(LoginViewModel loginViewModel)
     {
         // Here we will redirect back to whatever the redirect URL is.
