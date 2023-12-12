025094846a01f1bea7cd8e9bc018850e1d0f97dc
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-12-12 07:54:18 +0400

    Update AuthController.cs

--- 6-authentication-and-authorization/custom-identity-provider/WebApplication/Controllers/AuthController.cs
+++ 6-authentication-and-authorization/custom-identity-provider/WebApplication/Controllers/AuthController.cs
@@ -15,14 +15,14 @@ public class AuthController : Controller
     {
         // *** Example of the Return URL ***
         // /connect/authorize/callback?client_id=my_client_id_mvc
-        //& redirect_uri=https%3A%2F%2Flocalhost%3A7240%2Fsignin-oidc
-        //& response_type=code
-        //& scope=openid%20profile
-        //& code_challenge=74ut48tsVsebW9k9UAiR8YJK0NitTONmlEFyqScCAAw
-        //& code_challenge_method=S256
-        //& response_mode=form_post
-        //& nonce=638379231249474333.NDk5YjM3MGEtZTgwMy00Y2JjLWI0NDYtOGYzZGY4ZDZjZjRkOGU1
-        //& state=CfDJ8Gbrtnk6qZFAsrdxVOBx3dVok4BKle1zRKgTVU9LBpU8dWEDgdRVjkRRSTPotbmM0ve
+        //&redirect_uri=https%3A%2F%2Flocalhost%3A7240%2Fsignin-oidc
+        //&response_type=code
+        //&scope=openid%20profile
+        //&code_challenge=74ut48tsVsebW9k9UAiR8YJK0NitTONmlEFyqScCAAw
+        //&code_challenge_method=S256
+        //&response_mode=form_post
+        //&nonce=638379231249474333.NDk5YjM3MGEtZTgwMy00Y2JjLWI0NDYtOGYzZGY4ZDZjZjRkOGU1
+        //&state=CfDJ8Gbrtnk6qZFAsrdxVOBx3dVok4BKle1zRKgTVU9LBpU8dWEDgdRVjkRRSTPotbmM0ve
 
         return View(new LoginViewModel
         {
