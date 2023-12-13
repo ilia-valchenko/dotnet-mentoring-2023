dfe5bf9947a74ffb6f18b3f7fc959d88c9b1f203
Ilya.Valchanka <i.valchenk@hotmail.com>
2023-12-13 15:58:07 +0400

    Pushed some changes.

--- 6-authentication-and-authorization/custom-identity-provider/JavaScriptClientImplicitFlow/wwwroot/sign-in.js
+++ 6-authentication-and-authorization/custom-identity-provider/JavaScriptClientImplicitFlow/wwwroot/sign-in.js
@@ -12,19 +12,19 @@ var createNonce = function () {
 // to whatever we need to return.
 
 var signIn = function () {
-    var redirectUri = "https://localhost:7268/SignIn";
+    var redirectUri = "https://localhost:7268/Home/SignIn";
     var responseType = "id_token token";
     var scope = "openid catalog-api";
 
     // We are encoding a navigation to the Authorization endpoint.
     var authUrl =
-`/connect/authorize/callback
-?client_id=client_id_js
-?redirect_uri=${encodeURIComponent(redirectUri)}
-?response_type=${encodeURIComponent(responseType)}
-?scope=${encodeURIComponent(scope)}
-?nonce=${createNonce()}
-?session=${createSession()}`;
+"/connect/authorize/callback" +
+"?client_id=client_id_js" +
+"&redirect_uri=" + encodeURIComponent(redirectUri) +
+"&response_type=" + encodeURIComponent(responseType) +
+"&scope=" + encodeURIComponent(scope) +
+"&nonce=" + createNonce() +
+"&session=" + createSession();
 
     // FYI: response_mode is optional.
     // It something what belongs to the Authorization Code Flow
